2b52a47a6c1008dfa5367d2eeae3d8443adf85a1
Sadie Hale <sadie.m.hale@gmail.com>
2025-05-17 18:33:04 -0600

    phase 1 commit 2 - setTeamTurn and getTeamTurn

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -13,12 +13,12 @@ public class ChessGame {
     public ChessGame() {
 
     }
-
+    private TeamColor teamTurn;
     /**
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return teamTurn;
     }
 
     /**
@@ -27,7 +27,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        this.teamTurn = team;
     }
 
     /**
