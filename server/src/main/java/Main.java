3c3037ad2d8f7b0f4f362574d60a152d9443d9e6
Sadie Hale <sadie.m.hale@gmail.com>
2025-05-23 14:09:51 -0600

    phase 3 commit 2 - spark and server working

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,11 @@
 import chess.*;
+import spark.Spark;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+        Spark.port(8080);
+        Spark.get("/", (req, res) -> "Server working on port 8080");
     }
 }
