a03ed9322085c34e760ecebf5da4f6b2bd1369d3
Sadie Hale <sadie.m.hale@gmail.com>
2025-05-23 16:00:43 -0600

    phase 3 commit 3 - getting structured

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,11 +1,15 @@
 import chess.*;
 import spark.Spark;
-
+import java.util.UUID;
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
         Spark.port(8080);
         Spark.get("/", (req, res) -> "Server working on port 8080");
+    }
+
+    public static String generateToken(){
+        return UUID.randomUUID().toString(); //generates random authToken
     }
 }
